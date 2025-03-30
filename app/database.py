29b4d3485f4445322cc8960f983a93179c41311e
Brunno Viegas <58340417+brunno001v@users.noreply.github.com>
2025-03-29 21:38:46 -0300

    Update database.py
    Configuração da conexão com o banco de dados

--- app/database.py
+++ app/database.py
@@ -1,15 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Configuração do banco de dados
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db:5432/conecta"
+# Obtém a URL do banco do ambiente
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Configura o engine do SQLAlchemy
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 # Dependency para obter a sessão do DB
 def get_db():
